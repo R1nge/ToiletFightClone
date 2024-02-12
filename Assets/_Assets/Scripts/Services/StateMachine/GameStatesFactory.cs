efd82839c24efc7348402751e0815f47034025b5
R1nge <r1ngegamedev@gmail.com>
2024-02-12 10:02:53 +0300

    Fixed VContainer root installer not working (Need to fix in the template too)

--- Assets/_Assets/Scripts/Services/StateMachine/GameStatesFactory.cs
+++ Assets/_Assets/Scripts/Services/StateMachine/GameStatesFactory.cs
@@ -1,12 +1,20 @@
 ﻿using _Assets.Scripts.Services.StateMachine.States;
+using _Assets.Scripts.Services.UIs.StateMachine;
 
 namespace _Assets.Scripts.Services.StateMachine
 {
     public class GameStatesFactory
     {
+        private readonly UIStateMachine _uiStateMachine;
+
+        private GameStatesFactory(UIStateMachine uiStateMachine)
+        {
+            _uiStateMachine = uiStateMachine;
+        }
+        
         public IGameState CreateGameState(GameStateMachine stateMachine)
         {
-            return new GameState(stateMachine);
+            return new GameState(stateMachine, _uiStateMachine);
         }
     }
 }
--- Assets/_Assets/Scripts/Services/StateMachine/States/GameState.cs
+++ Assets/_Assets/Scripts/Services/StateMachine/States/GameState.cs
@@ -1,12 +1,22 @@
-﻿namespace _Assets.Scripts.Services.StateMachine.States
+﻿using _Assets.Scripts.Services.UIs.StateMachine;
+
+namespace _Assets.Scripts.Services.StateMachine.States
 {
     public class GameState : IGameState
     {
         private readonly GameStateMachine _stateMachine;
+        private readonly UIStateMachine _uiStateMachine;
 
-        public GameState(GameStateMachine stateMachine) => _stateMachine = stateMachine;
+        public GameState(GameStateMachine stateMachine, UIStateMachine uiStateMachine)
+        {
+            _stateMachine = stateMachine;
+            _uiStateMachine = uiStateMachine;
+        }
 
-        public void Enter() { }
+        public void Enter()
+        {
+            _uiStateMachine.SwitchState(UIStateType.Game);
+        }
 
         public void Exit() { }
     }
